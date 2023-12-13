7a69a55de19d3a60a293f2d6b36b2cc0aaaf1484
L <globe_909@hotmail.com>
2023-12-13 10:51:53 +0100

    Put the code for calling mom n dad

--- app/src/main/java/fr/isep/familyproject/PhoneCalls.kt
+++ app/src/main/java/fr/isep/familyproject/PhoneCalls.kt
@@ -1,5 +1,35 @@
 package fr.isep.familyproject
 
-class PhoneCalls {
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.widget.Button
 
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Find the Button using findViewById
+        val btnCallDad: Button = findViewById(R.id.btnCallDad)
+        val btnCallMom: Button = findViewById(R.id.btnCallMom)
+
+        btnCallDad.setOnClickListener {
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:93232323")
+
+
+            startActivity(dialIntent)
+        }
+
+        btnCallMom.setOnClickListener {
+
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:93232323")
+
+            startActivity(dialIntent)
+        }
+    }
 }
